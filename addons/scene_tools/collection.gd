a261de75564c5d9543da1df57b30983c3a716fd4
Artemy Fedotov <catlovescpp@gmail.com>
2024-05-26 11:36:22 +0400

    Add comment with asset dictionary structure

--- addons/scene_tools/collection.gd
+++ addons/scene_tools/collection.gd
@@ -2,6 +2,7 @@
 extends Resource
 
 @export var name: String = ""
+# Asset dictionary: "uid": String, "name": String, "thumbnail": PortableCompressedTexture2D
 @export var assets: Array[Dictionary] = []
 
 func _init(_name: String = "") -> void:
